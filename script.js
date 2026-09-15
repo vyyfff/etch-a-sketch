ef400e38d0c9507e6a526e6d105726df4ca774aa
vyyfff <vfedyay@protonmail.com>
2026-09-15 23:15:50 +0300

    Added grid size change + clear + random box color

--- script.js
+++ script.js
@@ -1,19 +1,67 @@
 "use strict";
 
 const container = document.querySelector(".container");
-const boxCount = 256;
+const sizeButton = document.querySelector("#size-btn");
+const clearButton = document.querySelector("#clear-button");
+let gridSize = 16;
+let gridFlexBasis = 0;
 
-const htmlString = Array(boxCount)
-  .fill()
-  .map((_, i) => `<div class="my-box" data-id="${i + 1}"></div>`)
-  .join("");
+function addGrid() {
+  const htmlString = Array(gridSize ** 2)
+    .fill()
+    .map((_, i) => `<div class="my-box" data-id="${i + 1}"></div>`)
+    .join("");
 
-container.insertAdjacentHTML("beforeend", htmlString);
+  container.insertAdjacentHTML("beforeend", htmlString);
+}
+
+function changeGridSize(newGridSize) {
+  while (true) {
+    newGridSize = prompt("Enter new size of the grid (1 - 100)");
+
+    if (newGridSize == null) {
+      return;
+    } else if (+newGridSize > 0 && +newGridSize <= 100) {
+      break;
+    } else {
+      alert("Incorrect value. Try again");
+    }
+  }
+  gridSize = newGridSize;
+
+  container.innerHTML = "";
+  addGrid(gridSize);
+
+  gridFlexBasis = 100 / gridSize;
+  document.documentElement.style.setProperty(
+    "--grid-flex-basis",
+    `${gridFlexBasis}%`,
+  );
+}
+
+function randomColor() {
+  let array = Array(3)
+    .fill()
+    .map((_, i) => Math.floor(Math.random() * 256))
+    .join(", ");
+  return array;
+}
 
 container.addEventListener("mouseover", (event) => {
   const box = event.target.closest(".my-box");
 
   if (box && container.contains(box)) {
-    box.style.backgroundColor = "black";
+    box.style.backgroundColor = `rgb(${randomColor()})`;
   }
 });
+
+sizeButton.addEventListener("click", () => {
+  changeGridSize();
+});
+
+clearButton.addEventListener("click", () => {
+  container.innerHTML = "";
+  addGrid(gridSize);
+});
+
+addGrid(gridSize);
